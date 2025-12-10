a7256f28a7baff8f23ca3652628c1e0ff8f06e8c
Jaseem Rahman <106687804+jaseemrahman@users.noreply.github.com>
2025-12-10 17:20:43 +0530

    Remove database configuration from settings
    Removed database configuration from environment variables.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -118,11 +118,6 @@ DATABASES = {
     )
 }
 
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
